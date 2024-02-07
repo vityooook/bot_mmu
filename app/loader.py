9bbf5477e2532c7f8433cd591d7a2c7b97636150
vityooook <vityooook@gmail.com>
2024-02-07 21:24:21 +0300

    add notification

--- app/loader.py
+++ app/loader.py
@@ -1,10 +1,18 @@
 from loguru import logger
-
+from notifiers.logging import NotificationHandler
 from aiogram import Bot, Dispatcher
 from aiogram.enums import ParseMode
 from aiogram.fsm.storage.memory import MemoryStorage
 
 import config
+
+# * if something happen, bot send a notification to admin
+param = {
+    'token': config.BOT_TOKEN,
+    'chat_id': config.ADMIN
+}
+handler = NotificationHandler("telegram", defaults=param)
+logger.add(handler, level="ERROR")
 
 # * Set up logging, declare the bot and dispatcher
 logger.add(
